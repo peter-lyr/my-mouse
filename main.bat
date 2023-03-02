9cb03e77e00a936e3bda4fbd026c72bb3610ee82
peter-lyr <llydrp.ldp@gmail.com>
2023-03-02 16:44:27 +0800

    main.bat

--- main.bat
+++ main.bat
@@ -1,8 +1,7 @@
 @echo off
-taskkill /f /im main.exe
+taskkill /f /im my-mouse.exe
 taskkill /f /im autohotkey.exe
 taskkill /f /im globalMouse.exe
-Ahk2Exe.exe /icon icon\main.ico /base E:\Program_Files\AutoHotkey\AutoHotkeyA32.exe /in main.ahk /out main.exe
-start /MIN /B main.exe
-timeout /t 5
+Ahk2Exe.exe /icon icon\main.ico /base E:\Program_Files\AutoHotkey\AutoHotkeyA32.exe /in main.ahk /out my-mouse.exe
+start /MIN /B my-mouse.exe
 exit
